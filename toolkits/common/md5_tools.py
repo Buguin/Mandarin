b96371218eb09945b0de82fbd7228c0d9fde01fc
Buguin <wybuguin@hotmail.com>
2017-06-23 02:35:05 +0800

    Add the tools of MD5
    Signed-off-by: Buguin <wybuguin@hotmail.com>

--- toolkits/common/md5_tools.py
+++ toolkits/common/md5_tools.py
@@ -1,2 +1,39 @@
 # -*- codeing:utf-8 -*-
 # __author__ = 'Buguin'
+import hashlib
+import os
+
+
+def get_md5(file_path):
+    file_size = os.path.getsize(file_path) / 1024 / 1024
+    if file_size > 80:
+        return get_big_file_md5(file_path)
+    else:
+        return get_file_md5(file_path)
+
+
+def get_file_md5(file_path):
+    if os.path.isfile(file_path):
+        f = open(file_path, 'rb')
+        md5obj = hashlib.md5()
+        md5obj.update(f.read())
+        hash_code = md5obj.hexdigest()
+        f.close()
+        return str(hash_code).upper()
+    return None
+
+
+def get_big_file_md5(file_path):
+    if os.path.isfile(file_path):
+        md5obj = hashlib.md5()
+        maxbuf = 8192
+        f = open(file_path, 'rb')
+        while True:
+            buf = f.read(maxbuf)
+            if not buf:
+                break
+            md5obj.update(buf)
+        f.close()
+        hash_code = md5obj.hexdigest()
+        return str(hash_code).upper()
+    return None
--- toolkits/xmltools/write_xml_tools.py
+++ toolkits/xmltools/write_xml_tools.py
@@ -4,32 +4,32 @@ import os
 
 from lxml import etree as et
 
+from toolkits.common.md5_tools import get_md5
+
 
 def get_source_infor(compare_tool):
     walk_file_path = compare_tool.source_abspath
     # with open(source_xmlpath, 'w') as f:
     #     f.write(doc.toprettyxml(indent='\t', encoding='utf-8'))
     file_num = 1
-    print(os.getcwd())
-    source_xmlpath = os.getcwd() + r"\\logs\\TargetItems\\" + compare_tool.name + ".xml"
+    file_name = compare_tool.offering + " " + compare_tool.version
+    source_xmlpath = os.getcwd() + r"\logs\ConfigItems\\" + file_name + ".xml"
     root = et.Element('targetitem')
     files = et.SubElement(root, 'files')
     for parent, dir_names, file_names in os.walk(walk_file_path):
         for file_name in file_names:
-            filepath = os.path.join(parent, file_name)
-            # filepath = os.path.join(parent, file_name).decode('utf-8')
+            file_path = os.path.join(parent, file_name)
             filedict = {'ID': str(file_num), 'md5': "", 'brother': '', 'child': '', 'parent': '', 'type': ''}
-            filedict.update({'md5': "MMMMMMMMMMMMMMMMM"})
+            filedict.update({'md5': get_md5(file_path)})
             file = et.SubElement(files, 'file', filedict)
-            file.text = filepath
+            file.text = file_path
             file_num += 1
             print("parent is:" + parent)
             print("filename is:" + file_name)
-            print("the full name of the file is:" + filepath)
+            print("the full name of the file is:" + file_path)
     files.set("num", str(file_num))
     tree = et.ElementTree(root)
     tree.write(source_xmlpath, encoding="utf-8", xml_declaration="utf-8", pretty_print=True)
-    print()
     return source_xmlpath
 
 
